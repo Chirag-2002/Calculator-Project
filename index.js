a83de8829fd03dca84d3f0495d0098ec8cf3d674
Chirag jain <83218348+Chirag-2002@users.noreply.github.com>
2022-02-24 17:10:21 +0530

    Add files via upload
    Javascript file is changed  I have use exception handling to handle the error

--- index.js
+++ index.js
@@ -24,56 +24,19 @@ for (item of compute) {
             displayvalue+=buttonText
             result.value=displayvalue;
         }
+        try
+        {
+            if  (buttonText=="=")
+            {
+                displayvalue+=buttonText;
+                result.value=eval(displayvalue)
+            }
+
+        }
+        catch(error)
+        {
+            result.value="Can't divide any number by zero ";
+        }
     });
 }
-
-
-
-
-
-
-
-
-
-
-// var form=document.getElementById('form');
-// var result=document.getElementsByName('ans');
-// var compute =document.getElementsByName('btn');
-// for (item of compute)
-// {
-//     item.addEventListener('click',buttonClick);
-// }
-
-// function buttonClick(e)
-//  {
-//     // console.log("Button Clicked !")
-//     // document.getElementById("heading").textContent="Perform";
-//     // result.innerHTML ='<h3>'+e.target+'</h3>'; // Dosen't Work
-//     // console.log(e.clientX);
-//     // console.log(e.altKey);
-//     //console.log(e.ctrlKey);
-//     //console.log(e.shiftKey);
-//     buttonText=e.target.value;
-//     // console.log(buttonText)
-//     if (buttonText=='X')
-//     {
-//         buttonText='*';
-//         result.value=buttonText;
-//     }
-// }
-// // TYPES OF EVENT IN JS :
-
-// // 1)click event
-// // 2) dblclick
-// // 3) mousedown
-// // 4) mouseup
-// // 5) mouseenter
-// // 6) mouseleave
-// // 7) mouseover
-// // 8) mouseout
-// //9) mousemove
-
-
-
-
 
